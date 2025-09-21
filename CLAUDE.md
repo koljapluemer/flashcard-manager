32681a36c6a876255b5bd6254da78525f371595d
Kolja Sam <github@koljapluemer.com>
2025-09-21 16:46:37 +0200

    fix pdf generation

--- CLAUDE.md
+++ CLAUDE.md
@@ -5,3 +5,5 @@
 - Keep code clean and extensible. Functions should be short, side-effect free when possible and do one thing only.
 - Keep files short; think of a smart folder/app structure
 - Use Poetry for package management.
+
+DO NOT RUN THE FUCKING DEV SERVER!!!!!
--- flashcard_collections/utils.py
+++ flashcard_collections/utils.py
@@ -1,64 +1,22 @@
-from reportlab.lib.pagesizes import letter
-from reportlab.lib.units import inch
-from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, PageBreak
-from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.enums import TA_CENTER
+from django.template.loader import render_to_string
+from weasyprint import HTML
 from io import BytesIO
-import re
-
-
-def clean_html(html_text):
-    """Remove HTML tags for PDF generation"""
-    clean_text = re.sub('<[^<]+?>', '', html_text)
-    clean_text = clean_text.replace('&nbsp;', ' ')
-    clean_text = clean_text.replace('&amp;', '&')
-    clean_text = clean_text.replace('&lt;', '<')
-    clean_text = clean_text.replace('&gt;', '>')
-    return clean_text.strip()
 
 
 def generate_pdf(collection):
-    """Generate business card sized PDF for flashcards - proper front/back layout for printing"""
-    buffer = BytesIO()
-
-    # Business card size: 3.5" x 2" (standard US business card)
-    card_width = 3.5 * inch
-    card_height = 2 * inch
-
-    doc = SimpleDocTemplate(buffer, pagesize=(card_width, card_height),
-                           topMargin=0.15*inch, bottomMargin=0.15*inch,
-                           leftMargin=0.15*inch, rightMargin=0.15*inch)
-
-    styles = getSampleStyleSheet()
-
-    # Custom style for flashcard content
-    card_style = ParagraphStyle(
-        'Card',
-        parent=styles['Normal'],
-        fontSize=10,
-        alignment=TA_CENTER,
-        spaceAfter=0,
-        leading=12,
-        wordWrap='CJK'
-    )
-
-    story = []
+    """Generate business card sized PDF for flashcards using WeasyPrint - preserves HTML formatting"""
     flashcards = list(collection.flashcards.all())
 
-    for flashcard in flashcards:
-        # FRONT of flashcard (odd page)
-        front_text = clean_html(flashcard.front)
-        story.append(Paragraph(front_text, card_style))
-        story.append(PageBreak())
+    # Render HTML template with flashcard data
+    html_content = render_to_string('collections/flashcard_pdf.html', {
+        'collection': collection,
+        'flashcards': flashcards
+    })
 
-        # BACK of flashcard (even page - will be on reverse when printed duplex)
-        back_text = clean_html(flashcard.back)
-        story.append(Paragraph(back_text, card_style))
+    # Generate PDF from HTML using WeasyPrint
+    html_doc = HTML(string=html_content)
+    pdf_buffer = BytesIO()
+    html_doc.write_pdf(pdf_buffer)
 
-        # Add page break unless this is the last card
-        if flashcard != flashcards[-1]:
-            story.append(PageBreak())
-
-    doc.build(story)
-    buffer.seek(0)
-    return buffer.getvalue()
+    pdf_buffer.seek(0)
+    return pdf_buffer.getvalue()
